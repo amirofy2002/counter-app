593477f88cd4094e50fc287deefdcf47318b5a4c
Amir Mehrabi <amir.mehrabi.j@gmail.com>
2018-10-20 00:32:39 +0330

    event emit to parent and the parent updates childrens

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,10 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-    Address: {
-      street: ""
-    },
-    imgUrl: "http://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"]
-  };
-
   styles = {
     fontSize: 20,
     fontWeight: "bold"
   };
 
-  doHandleClick = cnt => {
-    this.setState({ value: this.state.value + cnt });
-  };
-  self;
   //   constructor() {
   //     super();
   //     this.handleClick = this.handleClick.bind(this);
@@ -32,7 +19,7 @@ class Counter extends Component {
         </span>
         <button
           onClick={() => {
-            this.doHandleClick(1);
+            this.props.onIncrement(1);
           }}
           className="btn btn-secondary btn-sm"
         >
@@ -40,11 +27,19 @@ class Counter extends Component {
         </button>
         <button
           onClick={() => {
-            this.doHandleClick(-1);
+            this.props.onIncrement(-1);
           }}
           className="btn btn-info btn-sm"
         >
           -
+        </button>
+        <button
+          onClick={() => {
+            this.props.onDelete();
+          }}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
         {/* {this.renderTags()} */}
       </div>
@@ -62,16 +57,15 @@ class Counter extends Component {
     );
   }
 
-  getBadgeClasses() {
+  getBadgeClasses = () => {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.value === 0 ? "warning" : "primary";
     return classes;
-  }
+  };
 
-  formatCount() {
-    const { value } = this.state;
-    return this.state.value === 0 ? "Zero" : value;
-  }
+  formatCount = () => {
+    return this.props.value === 0 ? "Zero" : this.props.value;
+  };
 }
 
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -9,11 +9,52 @@ class Counters extends Component {
       { id: 3, value: 0, Title: "Motor" }
     ]
   };
+
+  handleDelete = x => {
+    console.log("event handler for delete called", x);
+    var idx = this.state.counters.filter(c => c.id !== x.id);
+    this.setState({ counters: idx });
+  };
+  handleIncrement = (x, i) => {
+    console.log("Handle increment on parent", x, i);
+    const countrs = [...this.state.counters];
+    const idx = countrs.indexOf(x);
+    countrs[idx] = x;
+    countrs[idx].value += i;
+    this.setState({ countrs });
+    //this.setState({ counters: this.counters });
+  };
+  handleReset = () => {
+    const rest = this.state.counters.map(x => {
+      x.value = 0;
+      return x;
+    });
+    this.setState({ counters: rest });
+  };
   render() {
     return (
       <div>
+        <button
+          className="btn btn-primary btn-sm m-2"
+          onClick={() => {
+            this.handleReset();
+          }}
+        >
+          Reset
+        </button>
         {this.state.counters.map(x => (
-          <Counter key={x.id} value={x.value} selected={true}>
+          <Counter
+            key={x.id}
+            onDelete={() => {
+              this.handleDelete(x);
+            }}
+            onIncrement={i => {
+              this.handleIncrement(x, i);
+            }}
+            value={x.value}
+            id={x.id}
+            selected={true}
+          >
             {x.Title}
           </Counter>
         ))}
