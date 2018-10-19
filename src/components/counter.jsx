911fcdbbb3ac733a328b76c6cdeed37b31ef7970
Amir Mehrabi <amir.mehrabi.j@gmail.com>
2018-10-19 23:26:39 +0330

    handle events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,6 +13,19 @@ class Counter extends Component {
     fontSize: 20,
     fontWeight: "bold"
   };
+  handleClick = () => {
+    console.info("increasing counter....", this);
+
+    this.setState({ count: this.state.count + 1 });
+  };
+  doHandleClick = cnt => {
+    this.setState({ count: this.state.count + cnt });
+  };
+  self;
+  //   constructor() {
+  //     super();
+  //     this.handleClick = this.handleClick.bind(this);
+  //   }
   render() {
     return (
       <React.Fragment>
@@ -20,13 +33,27 @@ class Counter extends Component {
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increments</button>
-        <ul>
-          {this.state.tags.map(t => (
-            <li key={t}>{t} </li>
-          ))}
-        </ul>
+        <button
+          onClick={() => {
+            this.doHandleClick(3);
+          }}
+          className="btn btn-secondary btn-sm"
+        >
+          Increments
+        </button>
+        {this.renderTags()}
       </React.Fragment>
+    );
+  }
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(t => (
+          <li key={t}>{t} </li>
+        ))}
+      </ul>
     );
   }
 
